1fddae9175297577c6e339abb9866b93a28a9e1b
Alexander Essenwanger <cibernop@mable-anex.de>
2024-04-16 20:03:43 +0000

    added more datapoints in the db init script

--- .devcontainer/init_db.sql
+++ .devcontainer/init_db.sql
@@ -15,5 +15,10 @@ CREATE TABLE readings (
 
 INSERT INTO readings (kind, recorded_on, reading) VALUES
     ('gas', '2023-01-01', 1234.56),
-    ('gas', '2023-01-02', 1235.67),
-    ('electricity', '2023-01-01', 456.8);
+    ('gas', '2023-01-08', 1235.67),
+    ('gas', '2023-01-15', 1242.67),
+    ('gas', '2023-01-22', 1248.67),
+    ('gas', '2023-01-29', 1252.67),
+    ('electricity', '2023-01-01', 456.8),
+    ('electricity', '2023-01-07', 526.8),
+    ('electricity', '2023-01-15', 612.8);
